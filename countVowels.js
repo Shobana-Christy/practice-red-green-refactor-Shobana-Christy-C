c96bed961888480abb00c941cb62e4b45e417c39
Shobana Christy <shobichristy07@gmail.com>
2025-09-13 22:22:10 -0500

    practiced red-green-refactor cycle

---
+++ countVowels.js
@@ -0,0 +1,38 @@
+let vowels = ["a", "e", "i", "o", "u"];
+
+// function countVowels(str) {
+//     let strAsArray = str.split("");
+//     let counter = 0;
+//     for (letter of strAsArray) {
+//         if (vowels.includes(letter)) {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+function countVowels(str) {
+    if(typeof str == "undefined") return undefined;
+    if(typeof str == "number" ) return 0;
+    if(typeof str == "boolean") return 0;
+    let strAsArray = str.split("");
+    let count = 0;
+    strAsArray.forEach(alp => {
+        // console.log(alp);
+        if (vowels.includes(alp))
+            count++;
+    });
+    return count;
+}
+
+
+//  console.log(countVowels(190));
+// console.log(countVowels("hello"));
+// console.log(countVowels(""));
+// console.log(countVowels("The sky is the limit"));
+// console.log(countVowels(true));
+// console.log(countVowels(false));
+// console.log(countVowels());
+
+module.exports = {
+    countVowels
+};
---
+++ test/countVowels.test.js
@@ -0,0 +1,71 @@
+// negative case
+// no vowels
+    // plrt = 0
+// empty string
+
+const countTest  = require("../countVowels");
+
+    // "" = 0
+describe("the count on vowel function ", function(){
+
+    test("should return the total count of vowels in the word", function(){
+        let str = "hello";
+        let received = countTest.countVowels(str);
+        let expected = 2;
+        expect(received).toEqual(expected);
+    });
+
+    test("should return the total count of vowels in the sentence", function(){
+        let str = "The sky is the limit";
+        let received = countTest.countVowels(str);
+        let expected = 5;
+        expect(received).toEqual(expected);
+    });
+
+    test("should return 0 when there is no vowels in the word", function(){
+        let str = "sky";
+        let received = countTest.countVowels(str);
+        let expected = 0;
+        expect(received).toEqual(expected);
+    });
+
+    test("should return 0 when there is empty space in the string", function(){
+        let str = "";
+        let received = countTest.countVowels(str);
+        let expected = 0;
+        expect(received).toEqual(expected);
+    });
+
+    test("should return 0 when number is given instead of string", function(){
+        let str = 22;
+        let received = countTest.countVowels(str);
+        let expected = 0;
+        expect(received).toEqual(expected);
+    });
+
+    test("should return 0 when boolean value true is given instead of string", function(){
+        let str = true;
+        let received = countTest.countVowels(str);
+        let expected = 0;
+        expect(received).toEqual(expected);
+    });
+     
+    test("should return 0 when boolean value false is given instead of string", function(){
+        let str = false;
+        let received = countTest.countVowels(str);
+        let expected = 0;
+        expect(received).toEqual(expected);
+    });
+
+    test("should return undefined when no value is given", function(){
+        let received = countTest.countVowels();
+        let expected = undefined;
+        expect(received).toEqual(expected);
+    });
+
+
+    });
+
+    
+
+
