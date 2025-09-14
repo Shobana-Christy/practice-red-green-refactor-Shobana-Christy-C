30875f36f0e5fba13a03f8e4f8d1cff8dd6d6b4f
Shobana Christy <shobichristy07@gmail.com>
2025-09-14 13:46:02 -0500

    practiced-red-green-refactor

--- countVowels.js
+++ countVowels.js
@@ -14,7 +14,7 @@ function countVowels(str) {
     if(typeof str == "undefined") return undefined;
     if(typeof str == "number" ) return 0;
     if(typeof str == "boolean") return 0;
-    let strAsArray = str.split("");
+    let strAsArray = str.toLowerCase().split("");
     let count = 0;
     strAsArray.forEach(alp => {
         // console.log(alp);
@@ -26,7 +26,7 @@ function countVowels(str) {
 
 
 //  console.log(countVowels(190));
-// console.log(countVowels("hello"));
+console.log(countVowels("Elephant"));
 // console.log(countVowels(""));
 // console.log(countVowels("The sky is the limit"));
 // console.log(countVowels(true));
--- test/countVowels.test.js
+++ test/countVowels.test.js
@@ -1,71 +1,79 @@
 // negative case
 // no vowels
-    // plrt = 0
+// plrt = 0
 // empty string
+// "" = 0
 
-const countTest  = require("../countVowels");
 
-    // "" = 0
-describe("the count on vowel function ", function(){
+const countTest = require("../countVowels");
 
-    test("should return the total count of vowels in the word", function(){
+
+describe("the count on vowel function ", function () {
+
+    test("should return the total count of vowels in the word", function () {
         let str = "hello";
         let received = countTest.countVowels(str);
         let expected = 2;
         expect(received).toEqual(expected);
     });
 
-    test("should return the total count of vowels in the sentence", function(){
+    test("should return the total count of vowels in the word when the string is in uppercase ", function () {
+        let str = "Elephant";
+        let received = countTest.countVowels(str);
+        let expected = 3;
+        expect(received).toEqual(expected);
+    });
+
+    test("should return the total count of vowels in the sentence", function () {
         let str = "The sky is the limit";
         let received = countTest.countVowels(str);
         let expected = 5;
         expect(received).toEqual(expected);
     });
 
-    test("should return 0 when there is no vowels in the word", function(){
+    test("should return 0 when there is no vowels in the word", function () {
         let str = "sky";
         let received = countTest.countVowels(str);
         let expected = 0;
         expect(received).toEqual(expected);
     });
 
-    test("should return 0 when there is empty space in the string", function(){
+    test("should return 0 when there is empty space in the string", function () {
         let str = "";
         let received = countTest.countVowels(str);
         let expected = 0;
         expect(received).toEqual(expected);
     });
 
-    test("should return 0 when number is given instead of string", function(){
+    test("should return 0 when number is given instead of string", function () {
         let str = 22;
         let received = countTest.countVowels(str);
         let expected = 0;
         expect(received).toEqual(expected);
     });
 
-    test("should return 0 when boolean value true is given instead of string", function(){
+    test("should return 0 when boolean value true is given instead of string", function () {
         let str = true;
         let received = countTest.countVowels(str);
         let expected = 0;
         expect(received).toEqual(expected);
     });
-     
-    test("should return 0 when boolean value false is given instead of string", function(){
+
+    test("should return 0 when boolean value false is given instead of string", function () {
         let str = false;
         let received = countTest.countVowels(str);
         let expected = 0;
         expect(received).toEqual(expected);
     });
 
-    test("should return undefined when no value is given", function(){
+    test("should return undefined when no value is given", function () {
         let received = countTest.countVowels();
         let expected = undefined;
         expect(received).toEqual(expected);
     });
 
+});
 
-    });
 
-    
 
 
